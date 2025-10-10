1649e4c165811c1e197a6ea2685b4392d2cc50f9
kamalahasank1ksk <kamalahasank003@gmail.com>
2025-10-10 15:31:57 +0530

    Create declare formats.py

---
+++ declare formats.py
@@ -0,0 +1,12 @@
+name="ajai"
+age=20
+
+print("my name is {} and i am {} years old".format(name,age))#last declare format
+
+print("my name is {0} and i am {1} years old".format(name,age))#index declare format
+
+print("my name is %s and i am %d years old"%(name,age)) #data type declare format
+
+num=0.156378
+print(f"result {num:.2f}") #digit formats
+print(f"result {num:.3f}")
