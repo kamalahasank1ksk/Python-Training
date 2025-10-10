9dee5b5009ccc95b2c77139aa5858c9355d9e27c
kamalahasank1ksk <kamalahasank003@gmail.com>
2025-10-10 14:55:25 +0530

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,10 @@
+class animal:
+    def __init__(self):
+        print("bark")
+        
+class dog(animal):
+    def nob(self):
+        print("bow bow")
+        
+d=dog()
+d.nob()
---
+++ function.py
@@ -0,0 +1,3 @@
+def greet(name):
+    print("Hello",name,"!!")
+greet(name=input("Enter friend Name :"))
